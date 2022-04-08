a21887d751af07f7a10a9fb1dd093c804445007a
root <eodkwokm@gmail.com>
2022-04-08 18:19:21 +0100

    uses websocket now

--- startup.py
+++ startup.py
@@ -1,5 +1,8 @@
 from db import mongo_connector
-from urllib.request import urlopen, Request
+import websocket
+import _thread
+import time
+import rel
 import json
 import logging
 
@@ -8,17 +11,30 @@ class BlockService:
     database = None
     currentBlock = 1
 
-    masternode = "https://masternode-01.lamden.io"
+    masternode = "wss://masternode-01.lamden.io"
+    websocket_connection = None
 
-    def getNewBlock(self, block_id):
-        block_info = urlopen(Request(self.masternode + "/blocks?num=" + str(block_id), headers={'User-Agent': 'Mozilla'}))
-        return json.loads(block_info.read())
+    def on_message(ws, message):
+        logging.debug(message)
+
+    def on_error(ws, error):
+        logging.debug(error)
+
+    def on_close(ws, close_status_code, close_msg):
+        logging.debug("Websocket connection closed")
+
+    def on_open(ws):
+        logging.debug("Opened websocket connection")
 
     def startup(self, port):
         self.blockservice_port = port
         self.database = mongo_connector.get_database()
-        logging.debug(self.getNewBlock(self.currentBlock))
-
+        self.websocket_connection = websocket.WebSocketApp(self.masternode,
+                              on_open=self.on_open,
+                              on_message=self.on_message,
+                              on_error=self.on_error,
+                              on_close=self.on_close)
+        self.websocket_connection.run_forever()
 
 if __name__ == "__main__":
     log = logging.getLogger("Blockservice")
