680e576cf3d1df95838f1b9917ac61dbbf593b3a
root <eodkwokm@gmail.com>
2022-04-08 17:23:32 +0100

    stuff

--- db/mongo_connector.py
+++ db/mongo_connector.py
@@ -1,1 +1,9 @@
 from pymongo import MongoClient
+
+def get_database():
+    CONNECTION_STRING = "mongodb://localhost:27017"
+    
+    client = MongoClient(CONNECTION_STRING)
+    
+    return client['blockservice_py']
+
--- startup.py
+++ startup.py
@@ -2,6 +2,7 @@ from db import mongo_connector
 
 class BlockService:
     blockservice_port = None
+    database = None
 
     masternode = "https://masternode-01.lamden.io"
 
@@ -10,6 +11,7 @@ class BlockService:
     
     def startup(self, port):
         self.blockservice_port = port
+        self.database = mongo_connector.get_database()
 
 blockservice = BlockService()
 blockservice.startup(port=1337)
